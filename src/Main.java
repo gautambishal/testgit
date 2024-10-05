0487793f3d9296b92e7b6e05f5be39ca40e8dfef
Bishal Gautam <bishalgautam@Bishals-MacBook-Pro.local>
2024-10-05 13:06:11 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        int a =20;
         System.out.println("Hello git!!");
     }
 }
@@ -9,5 +10,7 @@ public class Main {
 1. cross-collaboration
 2. versioning ->
 
+uncommited area -> commited area -> remote repo (Push ) github, bitbucket, ..gitlab..
+ 0--> 0
 
  */
